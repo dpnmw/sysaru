32fbf88eb8af2e3c2670e92a91cc1fe3f9c6be31
DPN MW <dpnmediaworks@gmail.com>
2026-03-12 13:53:39 -0400

    Undo Filter Change

--- admin/assets/javascripts/discourse/routes/admin-plugins-show-sysaru-app-cta.js
+++ admin/assets/javascripts/discourse/routes/admin-plugins-show-sysaru-app-cta.js
@@ -1,3 +1,11 @@
 import DiscourseRoute from "discourse/routes/discourse";
+import SiteSetting from "discourse/admin/models/site-setting";
+import SiteSettingFilter from "discourse/admin/lib/site-setting-filter";
 
-export default class AdminPluginsShowSysaruAppCtaRoute extends DiscourseRoute {}
+export default class AdminPluginsShowSysaruAppCtaRoute extends DiscourseRoute {
+  async model() {
+    const allSettings = await SiteSetting.findAll();
+    const filter = new SiteSettingFilter(allSettings);
+    return filter.filterSettings("sysaru_app_cta", {});
+  }
+}
--- admin/assets/javascripts/discourse/routes/admin-plugins-show-sysaru-topics.js
+++ admin/assets/javascripts/discourse/routes/admin-plugins-show-sysaru-topics.js
@@ -1,3 +1,11 @@
 import DiscourseRoute from "discourse/routes/discourse";
+import SiteSetting from "discourse/admin/models/site-setting";
+import SiteSettingFilter from "discourse/admin/lib/site-setting-filter";
 
-export default class AdminPluginsShowSysaruTopicsRoute extends DiscourseRoute {}
+export default class AdminPluginsShowSysaruTopicsRoute extends DiscourseRoute {
+  async model() {
+    const allSettings = await SiteSetting.findAll();
+    const filter = new SiteSettingFilter(allSettings);
+    return filter.filterSettings("sysaru_topics", {});
+  }
+}
